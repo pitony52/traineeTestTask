cec328a82584107be499561690e757548d545809
pitony52 <109213954+pitony52@users.noreply.github.com>
2022-07-15 22:12:24 +0300

    Update ReadFileTests.java

--- src/test/java/ReadFileTests.java
+++ src/test/java/ReadFileTests.java
@@ -1,9 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadFileTests {
 
     @Test
     public void secondLineShouldHabAlloha(){
-      //write here second task
+        //write here second task
+        try (FileReader reader = new FileReader("ReadFileTests.txt");
+             BufferedReader br = new BufferedReader(reader)) {
+            // read line by line
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.equals("аллоха")) {
+                    System.out.println("да, есть такое слово аллоха!");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
